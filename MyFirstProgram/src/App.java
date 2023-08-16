a7cc48877bfa308939920ee9b69becda33901dac
Lungelo Shabangu <lungelos@kutleng.co.za>
2023-08-16 14:33:37 +0200

    first commit - committing all files to the repository

---
+++ MyFirstProgram/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.print("This is my first Java program \n");
+
+        // This is a comment
+
+        /*
+         * This
+         * is
+         * a
+         * comment
+         */
+    }
+}
